cbdf2cb61ac9907e3123f54e2c13b0431e23ca80
Cindy Fang <cindyfang70@gmail.com>
2024-02-20 12:33:25 -0500

    add transfer_labels.R and man pages

---
+++ R/transfer_labels.R
@@ -0,0 +1,10 @@
+#' Transfer labels from the source dataset to the target dataset
+#'
+#' @param source A SingleCellExperiment or SpatialExperiment object with cell-type or spatial domain labels
+#' @param target A SingleCellExperiment or SpatialExperiment object to transfer labels into
+#'
+#' @return A SingleCellExperiment or SpatialExperiment objec
+#' @export
+transfer_labels <- function(source, target) {
+  warning("this is not yet implemented")
+}
