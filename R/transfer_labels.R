023c8eb5a27a9551133d99f92e9b590eaddbffcb
Cindy Fang <cindyfang70@gmail.com>
2024-02-22 13:22:55 -0500

    implement function for computing correlation between factors and labels

--- R/transfer_labels.R
+++ R/transfer_labels.R
@@ -3,8 +3,46 @@
 #' @param source A SingleCellExperiment or SpatialExperiment object with cell-type or spatial domain labels
 #' @param target A SingleCellExperiment or SpatialExperiment object to transfer labels into
 #'
-#' @return A SingleCellExperiment or SpatialExperiment objec
+#' @return A SingleCellExperiment or SpatialExperiment object
 #' @export
 transfer_labels <- function(source, target) {
   warning("this is not yet implemented")
+
+  # 1: run NMF on the source dataset
+  # 2: select the important factors based on correlation
+  # 3: fit multinomial model on source factors
+  # 4: project patterns onto target dataset
+  # 5: predict annotations on target factors
+}
+
+
+#' Compute correlation of each NMF factor with annotations in the source dataset
+#'
+#' @param nmf_model Outputted object from RcppML::nmf run on your source dataset.
+#' @param source_annotations A vector of per-cell annotations for your source dataset. For example, cell-type labels or sample ID.
+#'
+#' @return A NMF factor by unique annotations matrix of correlations between each factor and each unique annotation.
+#' @export
+compute_factor_correlations <- function(nmf_model, source_annotations) {
+  factors <- t(nmf_model$h)
+  colnames(factors) <- paste0("NMF", 1:k)
+
+  labels <- unique(source_annotations)
+  n_labels <-length(labels)
+  n_obs <- length(source_annotations)
+
+  factors.ind <- matrix(,nrow=n_obs, ncol=n_labels)
+  for (i in 1:n_labels){
+    label <- labels[[i]]
+    factors.ind[,i] <- as.integer(source_annotations == label)
+  }
+
+  colnames(factors.ind) <- labels
+  factors.ind <- as.data.frame(factors.ind)
+  cor.mat <- cbind(patterns, factors.ind)
+
+  M <- cor(cor.mat, use="complete.obs")
+  M <- M[grepl("NMF", rownames(M)), !grepl("NMF", colnames(M))]
+
+  return(M)
 }
