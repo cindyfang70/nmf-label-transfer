31df39b478a93baf48cccfd44bd87a6541470cc5
Cindy Fang <cindyfang70@gmail.com>
2024-02-26 17:05:36 -0500

    build out remaining transfer labels workflow

--- R/transfer_labels.R
+++ R/transfer_labels.R
@@ -24,10 +24,22 @@ transfer_labels <- function(source, target, assay, annotationsName, seed=123) {
                             seed=seed)
 
   # 2: select the important factors based on correlation
-  factor_annot_cors <- compute_factor_correlations(source_nmf_mod, colData(source)[[annotationsName]])
+  annots<- colData(source)[[annotationsName]]
+  factor_annot_cors <- compute_factor_correlations(source_nmf_mod, annots)
   factors_use <- identify_factors_representing_annotations(factor_annot_cors)
   # 3: fit multinomial model on source factors
+  multinom_mod <- fit_multinom_model(factors_use, annots)
   # 4: project patterns onto target dataset
+  projections <- project_factors(source, target, assay, source_nmf_mod)
   # 5: predict annotations on target factors
+
+  probs <- predict(multinom, newdata=projections, type='probs',
+                   na.action=na.exclude)
+
+  preds <- unlist(lapply(1:nrow(probs), function(xx){
+    colnames(probs)[which.max(probs[xx,])]
+  }))
+
+  return(preds)
 }
 
