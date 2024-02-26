e75e76b4a3965b12c1c268044251d502678095fe
Cindy Fang <cindyfang70@gmail.com>
2024-02-26 16:22:21 -0500

    move correlation function to new script

--- R/transfer_labels.R
+++ R/transfer_labels.R
@@ -2,47 +2,32 @@
 #'
 #' @param source A SingleCellExperiment or SpatialExperiment object with cell-type or spatial domain labels
 #' @param target A SingleCellExperiment or SpatialExperiment object to transfer labels into
+#' @param assay Name of the assay in `source` to use for NMF
+#' @param annotationsName Name of the annotations in `source` as found in the `colData`
+#' @param seed A random seed
 #'
 #' @return A SingleCellExperiment or SpatialExperiment object
+#'
+#' @import SpatialExperiment
+#' @import SummarizedExperiment
+#' @import methods
 #' @export
-transfer_labels <- function(source, target) {
-  warning("this is not yet implemented")
+transfer_labels <- function(source, target, assay, annotationsName, seed=123) {
+
+  if(is(source, "SpatialExperiment")){
+    stopifnot(assay %in% assays(source))
+    stopifnot(annotationsName %in% colnames(colData(source)))
+  }
 
   # 1: run NMF on the source dataset
+  source_nmf_mod <- run_nmf(data=source, assay="logcounts", k=100,
+                            seed=seed)
+
   # 2: select the important factors based on correlation
+  factor_annot_cors <- compute_factor_correlations(source_nmf_mod, colData(source)[[annotationsName]])
+  factors_use <- identify_factors_representing_annotations(factor_annot_cors)
   # 3: fit multinomial model on source factors
   # 4: project patterns onto target dataset
   # 5: predict annotations on target factors
 }
 
-
-#' Compute correlation of each NMF factor with annotations in the source dataset
-#'
-#' @param nmf_model Outputted object from RcppML::nmf run on your source dataset.
-#' @param source_annotations A vector of per-cell annotations for your source dataset. For example, cell-type labels or sample ID.
-#'
-#' @return A NMF factor by unique annotations matrix of correlations between each factor and each unique annotation.
-#' @export
-compute_factor_correlations <- function(nmf_model, source_annotations) {
-  factors <- t(nmf_model$h)
-  colnames(factors) <- paste0("NMF", 1:k)
-
-  labels <- unique(source_annotations)
-  n_labels <-length(labels)
-  n_obs <- length(source_annotations)
-
-  factors.ind <- matrix(,nrow=n_obs, ncol=n_labels)
-  for (i in 1:n_labels){
-    label <- labels[[i]]
-    factors.ind[,i] <- as.integer(source_annotations == label)
-  }
-
-  colnames(factors.ind) <- labels
-  factors.ind <- as.data.frame(factors.ind)
-  cor.mat <- cbind(patterns, factors.ind)
-
-  M <- cor(cor.mat, use="complete.obs")
-  M <- M[grepl("NMF", rownames(M)), !grepl("NMF", colnames(M))]
-
-  return(M)
-}
