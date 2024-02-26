a4fa7e59ad0bba71e0dedf73e4f99cb889852a48
Cindy Fang <cindyfang70@gmail.com>
2024-02-26 16:21:33 -0500

    add code for fitting multinomial model

---
+++ R/multinom_model.R
@@ -0,0 +1,19 @@
+#' Fit a multinomial modelto predict annotation using NMF factors as covariates.
+#'
+#' @param factors a matrix of factors from NMF
+#' @param source_annotations a vector of annotations
+#'
+#' @return
+#'
+#' @import nnet
+
+fit_multinom_model <- function(factors, source_annotations){
+  design <- cbind(annot=source_annotations, factors)
+
+  mod <-  nnet::multinom(annot ~ ., data = design,
+                   na.action=na.exclude, maxit=1000)
+
+  p.fit <- predict(mod, predictors=design[grepl("NMF", colnames(design))], type='probs')
+
+  return(mod)
+}
