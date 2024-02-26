4f1a1f1aadac4e4956b589e449f7c86b537d6cb9
Cindy Fang <cindyfang70@gmail.com>
2024-02-26 17:05:22 -0500

    add check for rank being less than number of columns and project_factors function

--- R/nmf_helpers.R
+++ R/nmf_helpers.R
@@ -25,10 +25,29 @@ run_nmf<- function(data, assay, k, seed=1237){
 }
 
 find_num_factors <- function(A, ranks = c(50,100,200)){
+  if(any(ranks >= ncol(A))){
+    stop("ranks must be less than the number of columns in A")
+    }
   cv <- singlet::cross_validate_nmf(A, ranks = ranks,
                                     n_replicates = 3,
                                     verbose=3)
   num_factors <- singlet::GetBestRank(cv)
 
   return(num_factors)
+}
+
+project_factors <- function(source, target, assay, nmf_model){
+  loadings <- nmf_model$w
+  #subset to the genes shared between the source and the target
+  i<-intersect(rownames(target),rownames(rowData(source)))
+  loadings<-loadings[rownames(loadings) %in% i,]
+  loadings<-loadings[match(rownames(target),rownames(loadings)),]
+
+  A <- assays(target, assay)
+  proj<-project(A=A, w=loadings, L1=0)
+
+  factors <- t(proj)/nmf_model$d #scale by the constant factor
+  colnames(factors) <- paste0("NMF", ncol(factors))
+
+  return(factors)
 }
