c687f84cf6dc1e9e69dff9e8ff40c16e14e91feb
Cindy Fang <cindyfang70@gmail.com>
2024-03-11 16:04:12 -0400

    updates

--- R/transfer_labels.R
+++ R/transfer_labels.R
@@ -15,7 +15,7 @@
 transfer_labels <- function(source, target, assay, annotationsName, seed=123) {
 
   if(is(source, "SpatialExperiment")){
-    stopifnot(assay %in% assays(source))
+    stopifnot(assay %in% assayNames(source))
     stopifnot(annotationsName %in% colnames(colData(source)))
   }
 
@@ -23,17 +23,27 @@ transfer_labels <- function(source, target, assay, annotationsName, seed=123) {
   source_nmf_mod <- run_nmf(data=source, assay="logcounts", k=100,
                             seed=seed)
 
+  source_factors <- t(source_nmf_mod$h)
+  colnames(source_factors) <- paste0("NMF", 1:ncol(source_factors))
+
   # 2: select the important factors based on correlation
   annots<- colData(source)[[annotationsName]]
-  factor_annot_cors <- compute_factor_correlations(source_nmf_mod, annots)
+  factor_annot_cors <- compute_factor_correlations(source_factors, annots)
   factors_use <- identify_factors_representing_annotations(factor_annot_cors)
+  print(factors_use)
   # 3: fit multinomial model on source factors
+
+
+  factors_use <- source_factors[,factors_use]
+  print(head(factors_use))
   multinom_mod <- fit_multinom_model(factors_use, annots)
+
   # 4: project patterns onto target dataset
   projections <- project_factors(source, target, assay, source_nmf_mod)
-  # 5: predict annotations on target factors
 
-  probs <- predict(multinom, newdata=projections, type='probs',
+   # 5: predict annotations on target factors
+
+  probs <- predict(multinom_mod, newdata=projections, type='probs',
                    na.action=na.exclude)
 
   preds <- unlist(lapply(1:nrow(probs), function(xx){
