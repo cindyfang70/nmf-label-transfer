7189d31c2ba9e332e8987c9741e46c2bacea3313
Cindy Fang <cindyfang70@gmail.com>
2024-03-11 16:03:59 -0400

    add setting RcppML.threads option otherwise project doesn't work

--- R/nmf_helpers.R
+++ R/nmf_helpers.R
@@ -11,15 +11,16 @@
 #' @import RcppML
 #' @export
 run_nmf<- function(data, assay, k, seed=1237){
-  A <- assays(data, assay)
+  A <- as.matrix(assay(data, assay))
+  print(k)
 
-  if(missing(k)){
+  if(is.null(k)){
     warning("Number of factors for NMF not specified. Using cross-validation to idenitfy optimal number of clusters")
       k <- find_num_factors(A)
   }
 
 
-  model <- RcppML::nmf(A, k = k, seed=seed)
+  model <- singlet::run_nmf(A, rank=k)
 
   return(model)
 }
@@ -37,17 +38,26 @@ find_num_factors <- function(A, ranks = c(50,100,200)){
 }
 
 project_factors <- function(source, target, assay, nmf_model){
+  if(is(target, "SpatialExperiment")){
+    stopifnot(assay %in% assayNames(target))
+  }
+
   loadings <- nmf_model$w
   #subset to the genes shared between the source and the target
-  i<-intersect(rownames(target),rownames(rowData(source)))
+  i<-intersect(rownames(target), rowData(source)$gene_name) # need to check for gene names in source object later
+  rownames(loadings) <- rowData(source)$gene_name
   loadings<-loadings[rownames(loadings) %in% i,]
   loadings<-loadings[match(rownames(target),rownames(loadings)),]
 
-  A <- assays(target, assay)
-  proj<-project(A=A, w=loadings, L1=0)
+
+  A <- assay(target, assay)
+  print(head(loadings))
+  options(RcppML.threads = 0) #line below doesn't work otherwise
+  proj<-RcppML::project(data=as.matrix(A), w=loadings, threads=0, L1=0, mask=NULL)
 
   factors <- t(proj)/nmf_model$d #scale by the constant factor
-  colnames(factors) <- paste0("NMF", ncol(factors))
+  print(head(factors))
+  colnames(factors) <- paste0("NMF", 1:ncol(factors))
 
   return(factors)
 }
