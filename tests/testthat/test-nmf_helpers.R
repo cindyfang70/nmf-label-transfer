ed5934b95114bfe0ef7db771cd2f279af785f2b0
Cindy Fang <cindyfang70@gmail.com>
2024-09-10 11:22:07 -0400

    use example() in tests instead of running new code

--- tests/testthat/test-nmf_helpers.R
+++ tests/testthat/test-nmf_helpers.R
@@ -1,57 +1,18 @@
+example(transfer_labels, echo = TRUE)
 test_that("outputted NMF factors are all nonnegative", {
-  # get the annotated source data
-  ehub <- ExperimentHub::ExperimentHub()
-  layer_labs <- "BayesSpace_harmony_09"
-  data_type <- "spatialDLPFC_Visium"
-  vis_anno <- spatialLIBD::fetch_data(type = data_type, eh = ehub)
-
-  vis_anno_sub <- vis_anno[,which(vis_anno$sample_id %in% unique(vis_anno$sample_id)[1:2])]
-
-  # get the unannotated target data
-  data(sfe)
-
-  source_nmf_mod <- run_nmf(data=vis_anno_sub, assay="logcounts", k=10,
-                            seed=123)
-
-  source_factors <- t(source_nmf_mod$h)
-
+  source_factors <- t(res$nmf$h)
   expect_equal(sum(source_factors >= 0), length(source_factors))
 })
 
 test_that("predicted probabilities are in [0,1]",{
-  # get the annotated source data
-  ehub <- ExperimentHub::ExperimentHub()
-  layer_labs <- "BayesSpace_harmony_09"
-  data_type <- "spatialDLPFC_Visium"
-  vis_anno <- spatialLIBD::fetch_data(type = data_type, eh = ehub)
-
-  vis_anno_sub <- vis_anno[,which(vis_anno$sample_id %in% unique(vis_anno$sample_id)[1:2])]
-
-  # get the unannotated target data
-  data(spe)
-  rowData(spe)$gene_name <- rownames(rowData(spe))
-
-  source_nmf_mod <- run_nmf(data=vis_anno_sub, assay="logcounts", k=10,
-                            seed=1237)
-
-  source_factors <- t(source_nmf_mod$h)
-  colnames(source_factors) <- paste0("NMF", 1:ncol(source_factors))
-
-  annots<- colData(vis_anno_sub)[[layer_labs]]
-  factor_annot_cors <- compute_factor_correlations(source_factors, annots)
-  technical_cors <- compute_factor_correlations(source_factors, colData(vis_anno_sub)[["sample_id"]])
-  factors_use <- identify_factors_representing_annotations(factor_annot_cors, technical_cors)
-
-  factors_use <- source_factors[,factors_use]
-  multinom_mod <- fit_multinom_model(factors_use, colData(vis_anno_sub)[[layer_labs]])
 
 
-  spe <- scuttle::logNormCounts(spe)
-  projections <- project_factors(vis_anno_sub, spe, assay="logcounts", source_nmf_mod)
+  vis_anno_target <- res$targets
+  multinom_mod <- res$multinom
+  projections <- reducedDim(vis_anno_target, "nmf_projections")
 
   probs <- predict(multinom_mod, newdata=projections, type='probs',
-                   na.action=na.exclude)
-
+                                    na.action=na.exclude)
   expect_equal(sum(probs >= 0 & probs <= 1), length(probs))
   expect_equal(as.vector(rowSums(probs)), rep(1, nrow(probs)))
 })
--- tests/testthat/test-transfer_labels.R
+++ tests/testthat/test-transfer_labels.R
@@ -1,22 +1,22 @@
+example(transfer_labels, echo = TRUE)
 test_that("single sample label transfer works", {
-  # get the annotated source data
-  ehub <- ExperimentHub::ExperimentHub()
-  layer_labs <- "BayesSpace_harmony_09"
-  data_type <- "spatialDLPFC_Visium"
-  vis_anno <- spatialLIBD::fetch_data(type = data_type, eh = ehub)
-
-  vis_anno_sub <- vis_anno[,which(vis_anno$sample_id %in% unique(vis_anno$sample_id)[c(1,2)])]
-
-  # get the unannotated target data
-  data(spe)
-  rowData(spe)$gene_name <- rownames(spe)
-  spe <- scuttle::logNormCounts(spe)
-
-  preds <- transfer_labels(targets=spe, source=vis_anno_sub, assay="logcounts", annotationsName=layer_labs,
-                           technicalVarName="sample_id", save_nmf=FALSE, k=10,
-                           cv_tol=1e-4, tol=1e-5)
-
-  #print(preds)
-
-  expect_true(length(preds$targets$nmf_preds)==ncol(spe))
+  # # get the annotated source data
+  # ehub <- ExperimentHub::ExperimentHub()
+  # layer_labs <- "BayesSpace_harmony_09"
+  # data_type <- "spatialDLPFC_Visium"
+  # vis_anno <- spatialLIBD::fetch_data(type = data_type, eh = ehub)
+  #
+  # vis_anno_sub <- vis_anno[,which(vis_anno$sample_id %in% unique(vis_anno$sample_id)[c(1,2)])]
+  #
+  # # get the unannotated target data
+  # data(spe)
+  # rowData(spe)$gene_name <- rownames(spe)
+  # spe <- scuttle::logNormCounts(spe)
+  #
+  # preds <- transfer_labels(targets=spe, source=vis_anno_sub, assay="logcounts", annotationsName=layer_labs,
+  #                          technicalVarName="sample_id", save_nmf=FALSE, k=10, tol=1e-5)
+  #
+  # #print(preds)
+  expect_equal(dim(vis_anno_target), dim(target_with_preds))
+  expect_true(length(res$targets$nmf_preds)==ncol(vis_anno_target))
 })
